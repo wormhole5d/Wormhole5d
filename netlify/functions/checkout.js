b000b193dca4d7bc45f6866492b9ff920a496f03
wormhole5d <robensley823475@gmail.com>
2025-10-30 17:41:25 -0500

    Create checkout.js

---
+++ netlify/functions/checkout.js
@@ -0,0 +1,16 @@
+// netlify/functions/checkout.js
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+exports.handler = async (event) => {
+  const { amount } = JSON.parse(event.body);
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: [{ price_data: { currency: 'usd', product_data: { name: 'Wormhole 5D Entry' }, unit_amount: amount }, quantity: 1 }],
+    mode: 'payment',
+    success_url: 'https://wormhole5d.github.io/Wormhole5d/success.html',
+    cancel_url: 'https://wormhole5d.github.io/Wormhole5d/',
+  });
+
+  return { statusCode: 200, body: JSON.stringify({ id: session.id }) };
+};
